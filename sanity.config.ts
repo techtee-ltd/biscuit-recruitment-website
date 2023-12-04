9ef655142d5fd66d7c76b5ff25f10404fdfdfbc9
samuelwondimu <wsamuelwondimu@gmail.com>
2023-12-04 16:08:11 +0300

    sanity: add filter and structure for sanity desk

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,46 @@
-// https://github.com/kapehe-ok/next-sanity-test/tree/main
-
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
+import {
+  StructureBuilder,
+  StructureResolver,
+  StructureResolverContext,
+  deskTool,
+} from "sanity/desk";
 import schemas from "./sanity/schema";
+
+const structure: StructureResolver = (
+  S: StructureBuilder,
+  context: StructureResolverContext
+) => {
+  return S.list()
+    .title("BISCUIT")
+    .items([
+      S.listItem()
+        .title("Active Jobs")
+        .child(
+          S.documentTypeList("job")
+            .title("Jobs by Status")
+            .filter("status == true")
+        ),
+      S.listItem()
+        .title("In-Active Jobs")
+        .child(
+          S.documentTypeList("job")
+            .title("Jobs by Status")
+            .filter("status == false")
+        ),
+      S.listItem().title("Consults").child(S.documentTypeList("consult")),
+      S.listItem()
+        .title("Journals")
+        .child(S.documentTypeList("journal").title("Journals")),
+      S.listItem()
+        .title("Privacy Policy")
+        .child(S.documentTypeList("privacyPolicy").title("Privacy Policy")),
+      S.listItem()
+        .title("Sector")
+        .child(S.documentTypeList("sector").title("Sector")),
+    ]);
+};
 
 const config = defineConfig({
   title: "Biscuit Recruitment",
@@ -11,7 +48,12 @@ const config = defineConfig({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-01",
   basePath: "/admin",
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      structure,
+    }),
+    visionTool(),
+  ],
   schema: { types: schemas },
 });
 
